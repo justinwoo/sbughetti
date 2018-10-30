7728e4c76e8307db1a221bbd27ab8445bb6fcef6
justinwoo <moomoowoo@gmail.com>
2018-10-30 23:08:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sbughetti
+
+A worse version of [spaghetto](https://github.com/justinwoo/spaghetto) written in Golang.
+
+This exists primarily to illustrate how things in Spacchetti/Psc-Package work and is not meant for normal use.
+
+See <https://github.com/justinwoo/spacchetti-cli> for the normal Spacchetti CLI.
+
---
+++ sbughetti.go
@@ -0,0 +1,213 @@
+package main
+
+import "flag"
+import "fmt"
+import "os"
+import "os/exec"
+import "strings"
+
+const dir string = ".spacchetti"
+const config string = dir + "/spacchetti.json"
+
+func ensureSpacchettiConfig() {
+	_, existsErr := os.Stat(config)
+
+	if existsErr == nil {
+		return
+	}
+
+	cmd := exec.Command(
+		"bash",
+		"-c",
+		"dhall-to-json <<< ./spacchetti.dhall > "+config,
+	)
+
+	execErr := cmd.Run()
+
+	if execErr != nil {
+		panic(execErr)
+	}
+}
+
+func jqConfig(expression string) string {
+	cmd := exec.Command(
+		"jq",
+		expression,
+		"-r",
+		config,
+	)
+
+	output, execErr := cmd.CombinedOutput()
+
+	if execErr != nil {
+		panic(execErr)
+	}
+
+	return string(output)
+}
+
+func getKeys(values map[string]bool) []string {
+	// i dont know where this is in the stdlib
+	keys := make([]string, len(values))
+
+	i := 0
+	for k := range values {
+		keys[i] = k
+		i++
+	}
+
+	return keys
+}
+
+func mkdirAll(path string) {
+	weirdErr := os.MkdirAll(path, 0755)
+
+	if weirdErr != nil {
+		panic(weirdErr)
+	}
+}
+
+func getProperty(dep string, property string) string {
+	result := jqConfig(".packages.\"" + dep + "\".\"" + property + "\"")
+	return strings.TrimSpace(result)
+}
+
+func getTransitiveDepsOf(dep string, visited map[string]bool) {
+	if visited[dep] == true {
+		return
+	}
+
+	visited[dep] = true
+
+	output := jqConfig(".packages.\"" + dep + "\".dependencies[]")
+
+	xs := strings.Fields(output)
+
+	for _, x := range xs {
+		getTransitiveDepsOf(x, visited)
+	}
+}
+
+func getDeps() []string {
+	output := jqConfig(".dependencies[]")
+
+	directDeps := strings.Fields(output)
+
+	var visited = make(map[string]bool)
+
+	for _, dep := range directDeps {
+		getTransitiveDepsOf(dep, visited)
+	}
+
+	keys := getKeys(visited)
+
+	return keys
+}
+
+func getTargetDir(dep string, version string) string {
+	return fmt.Sprintf("%s/%s/%s", dir, dep, version)
+}
+
+func installDep(dep string) {
+	repo := getProperty(dep, "repo")
+	version := getProperty(dep, "version")
+
+	targetDir := getTargetDir(dep, version)
+
+	_, existsErr := os.Stat(targetDir)
+
+	if existsErr == nil {
+		return
+	}
+
+	fmt.Println("installing", targetDir)
+
+	cmd := exec.Command(
+		"git",
+		"clone",
+		"-c",
+		"advice.detachedHead=false",
+		"--branch",
+		version,
+		repo,
+		targetDir,
+	)
+
+	err := cmd.Run()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getSources() []string {
+	ensureSpacchettiConfig()
+	deps := getDeps()
+
+	var globs = make(map[string]bool)
+
+	for _, dep := range deps {
+		version := getProperty(dep, "version")
+		targetDir := getTargetDir(dep, version)
+		glob := fmt.Sprintf("%s/src/**/*.purs", targetDir)
+		globs[glob] = true
+	}
+
+	keys := getKeys(globs)
+
+	return keys
+}
+
+func main() {
+	var init bool
+	var install bool
+	var sources bool
+	var build bool
+
+	flag.BoolVar(&init, "init", false, "initialize a Spacchetti project in this repo.")
+	flag.BoolVar(&install, "install", false, "install the project dependencies.")
+	flag.BoolVar(&sources, "sources", false, "get the source globs for the project.")
+	flag.BoolVar(&build, "build", false, "buildFn the project.")
+	flag.Parse()
+
+	// make sure we have dir to work with
+	mkdirAll(dir)
+
+	if install {
+		ensureSpacchettiConfig()
+		deps := getDeps()
+
+		fmt.Println("Installing", len(deps), "dependencies.")
+
+		for _, dep := range deps {
+			installDep(dep)
+		}
+	}
+	if sources {
+		sources := getSources()
+
+		for _, x := range sources {
+			fmt.Println(x)
+		}
+	}
+	if build {
+		sources := getSources()
+
+		baseArgs := []string{"compile", "src/**/*.purs", "test/**/*.purs"}
+
+		args := append(baseArgs, sources...)
+
+		cmd := exec.Command(
+			"purs",
+			args...,
+		)
+
+		execErr := cmd.Run()
+
+		if execErr != nil {
+			panic(execErr)
+		}
+
+		fmt.Println("Build succeeded.")
+	}
+}
